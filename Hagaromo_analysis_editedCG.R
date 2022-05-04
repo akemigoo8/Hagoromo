7ff0ed25be9e451790c3dee632234e6751d45596
akemigoo8 <goocam21@uw.edu>
2022-05-04 07:53:47 -0700

    combined data

--- Hagaromo_analysis_editedCG.R
+++ Hagaromo_analysis_editedCG.R
@@ -3,8 +3,7 @@
 author: "Tanya Brown"
 date: "3/11/2021"
 output: html_document
-# hehe
-# on
+#edit: Camille Goo 4/30/2022
 ---
   
   ```{r setup, include=FALSE}
@@ -12,6 +11,7 @@ knitr::opts_chunk$set(echo = TRUE)
 ```
 
 ```{r libraries}
+install.packages('dplyr')
 library(tidyverse)
 library(readxl)
 library(ggplot2)
@@ -24,20 +24,23 @@ library(dplyr)
 ```{r things }
 #set your working directory, where your file is located
 #setwd('/Users/tanyabrown/Rasmussen Lab/Data analysis') #set up your working directory
-setwd("C:/Users/goocam21/Desktop")
-#read in your file and specific sheet
-#read in your file: make sure that you have changed the sheet name and column name so it doesn't have spaces and column names match those in the function)
+##setwd("C:/Users/goocam21/Desktop")
+setwd("C:/Users/admin/Desktop/Hagaromo")
 
+##This script was written to read an Excel sheet. 
+#read in your file and specific sheet
+#read in your file: make sure that you have changed the sheet name and column name so it 
+#doesn't have spaces and column names match those in the function)
+
+##Draw plot of just CG large area data. 
 
 cghag_data <- read_excel("210311-hagaromo-MC.xlsx - Raw_data.xlsx",
-                         'CGfull',
-                       col_names = TRUE)
+                         'CGfull',  col_names = TRUE)
 head(cghag_data)
 
 #View(dist_data)
-
 ##hag_scale_data <-read_excel("210311-hagaromo-MC.xlsx - Raw_data.xlsx",
-                            'Combfull', col_names = TRUE)
+'Combfull', col_names = TRUE)
 ##head(hag_scale_data)
 ```
 
@@ -51,7 +54,7 @@ head(cghag_sep)
 ```
 
 ```{r graphs}
-cgg_order <- c("hag.sibs", "hag.mut")
+g_order <- c("hag.sibs", "hag.mut")
 
 cghag_sep %>% 
   ggplot(aes(x = factor(genotype, level = g_order), cell_density_mm2, fill = genotype)) +
@@ -75,7 +78,8 @@ cghag_sep %>%
   scale_color_brewer('set 2')
 
 #ggsave("/Users/tanyabrown/Rasmussen lab/Data analysis/filename.pdf", width = 6, height = 4, dpi = 300)
-ggsave("/Users/goocam21/Desktop/mm2xgenoCGlarge.pdf", width = 6, height = 4, dpi = 300)
+##ggsave("/Users/goocam21/Desktop/mm2xgenoCGlarge.pdf", width = 6, height = 4, dpi = 300)
+ggsave("/Users/admin/Desktop/Hagaromo/mm2xGeno_CGlarge.pdf", width = 6, height = 4, dpi = 300)
 
 ```
 ##Draw plot with TB and CG Large Image Data
@@ -84,41 +88,11 @@ hag_data<-read_excel("210311-hagaromo-MC.xlsx - Raw_data.xlsx",
                      col_names = TRUE)
 head(hag_data)
 
-g_order <- c("hag.sibs", "hag.mut")
-
-hag_sep %>% 
-  ggplot(aes(x = factor(genotype, level = g_order), cell_density_mm2, fill = genotype)) +
-  geom_boxplot()+
-  geom_jitter(width = 0.15)+
-  stat_compare_means()+
-  scale_y_continuous(expand = c(0,0),
-                     limits = c(0, 1600),
-                     breaks = seq(from = 0, to = 1600, by = 200))+
-  theme_bw()+
-  #theme(legend.position = 'none') +
-  theme(text = element_text(size = 20),
-        axis.text.x = element_text(size = 18),
-        axis.text.y = element_text(size = 18),
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(), 
-        panel.background = element_blank(),
-        axis.line = element_line(colour = 'black'))+
-  labs(title = 'zMC Cell Density', x = 'Genotype', y = 'zMC Density (cells per mm2)')+
-  theme(plot.title = element_text(hjust=0.5))+
-  scale_color_brewer('set 2')
-
-```
-
-```{r cars}
-hag_sep <- cghag_data %>%
+hag_sep <- hag_data %>%
   separate('fish_name',
            into = c('exp_date', 'transgene', 'genotype', 'fish_id'), 
            sep = "-" )
 
-head(hag_sep)
-```
-
-```{r graphs}
 g_order <- c("hag.sibs", "hag.mut")
 
 hag_sep %>% 
@@ -141,14 +115,21 @@ hag_sep %>%
   labs(title = 'zMC Cell Density', x = 'Genotype', y = 'zMC Density (cells per mm2)')+
   theme(plot.title = element_text(hjust=0.5))+
   scale_color_brewer('set 2')
+
+##ggsave("/Users/goocam21/Desktop/mm2xgenoCGlarge.pdf", width = 6, height = 4, dpi = 300)
+ggsave("/Users/admin/Desktop/Hagaromo/mm2xGeno_CGlarge.pdf", width = 6, height = 4, dpi = 300)
+
+##to get graphs for Small area data, go to line 204
+
+
 Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
 
 ```{r}
 #this function allows you to choose the genotypes you want to include in your analysis
-data<-hag_sep
+#{data<-hag_sep
 
 hag_subset_data <- function(data)
-data_sep <- data %>%
+  data_sep <- data %>%
   separate('fish_name',
            into = c('exp_date', 'transgene', 'genotype', 'fish_id'), 
            sep = "-" )
@@ -186,51 +167,94 @@ hag_plot <- data %>%
 #+ scale_color_brewer(palette = "Set11")
 return(hag_plot)
 hag_plot
-ggsave("/Users/goocam21/Desktop/mm2xSLTBCG.pdf", width = 6, height = 4, dpi = 300)
+ggsave("/Users/goocam21/Desktop/Hagaromo/mm2xSLTBCG.pdf", width = 6, height = 4, dpi = 300)}
 ```
 
 ```{r Johnson Neyman}
-#load the packages and libraries:
-#install.packages('interactions')
-#install.packages('jtools')
-#install.packages("sandwich")
-library(jtools)
-library(interactions)
-library(sandwich)
+##load the packages and libraries:
+##install.packages('interactions')
+##install.packages('jtools')
+##install.packages("sandwich")
+##library(jtools)
+##library(interactions)
+##library(sandwich)
 #exclude WT data from analysis
 
-hag_subset_data <- hag_data %>% separate('fish_name', 
-                                         into = c('exp_date', 'transgene', 'genotype', 'fish_id'), 
-                                         sep = "-")
-h_mut_sib_data <-filter(hag_subset_data, !grepl('hag.sibs', genotype))
+#hag_subset_data <- hag_data %>% separate('fish_name', 
+into = c('exp_date', 'transgene', 'genotype', 'fish_id'), 
+sep = "-")
+#h_mut_sib_data <-filter(hag_subset_data, !grepl('hag.sibs', genotype))
 
 #change the data so that genotype is a numerical value bc JN technique struggles with factor moderators
-hag_new_data <- hag_subset_data %>%
-  mutate(gen = if_else(genotype == 'hag.mut', 1, 0))
+#hag_new_data <- hag_subset_data %>%
+mutate(gen = if_else(genotype == 'hag.mut', 1, 0))
 
 #run your linear model and johnson-neyman stats
-mod6 <- lm(cell_density_mm2~fish_mm*gen, data=hag_new_data)
-h_ss <- sim_slopes(mod6, pred = gen, modx = fish_mm, johnson_neyman = TRUE)
-hag_jn <- johnson_neyman(mod6, pred = gen, modx = fish_mm, plot = TRUE) #show the JN plot
+#mod6 <- lm(cell_density_mm2~fish_mm*gen, data=hag_new_data)
+#h_ss <- sim_slopes(mod6, pred = gen, modx = fish_mm, johnson_neyman = TRUE)
+#hag_jn <- johnson_neyman(mod6, pred = gen, modx = fish_mm, plot = TRUE) #show the JN plot
 
 
 #show your plot and simple slopes analysis
-h_ss
+#h_ss
 
 ```
 
 ```{r stuff}
+##Draw plot of Small Area MC Density, CG then Combined data
 
-cgscale_data<-read_excel("22.04.29_hagsmalldataCG.xlsx",
-                         col_names = TRUE)
+CGhag_smalldata<-read_excel("210311-hagaromo-MC.xlsx - Raw_data.xlsx",
+                          'CGSmall',
+                          col_names = TRUE)
+head(CGhag_smalldata)
 
-cgscale_data <- hag_scale_data %>%
+CGhag_scale_sep <- CGhag_smalldata %>%
   separate('fish_name',
            into = c('exp_date', 'transgene', 'genotype', 'fish_id'), 
-           sep = "_" )
+           sep = "-" )
 
+CGhag_scale_sep
 g_order <- c("hag.sibs", "hag.mut")
 
+
+
+CGhag_scale_sep %>% 
+  ggplot(aes(x = factor(genotype, lev = g_order), scale_density, fill = genotype)) +
+  geom_boxplot()+
+  geom_jitter(width = 0.2)+
+  #stat_compare_means(label.y = 2000)+
+  scale_y_continuous(expand = c(0,0),
+                     limits = c(0, 2400),
+                     breaks = seq(from = 0, to = 2600, by = 400))+
+  theme_bw()+
+  theme(legend.position = 'none') +
+  theme(text = element_text(size = 20),
+        axis.text.x = element_text(size = 18),
+        axis.text.y = element_text(size = 18),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.background = element_blank(),
+        axis.line = element_line(colour = 'black'))+
+  labs(title = 'zMC Cell Density per Scale (Small area CG only)', x = 'Genotype', y = 'zMC Density (cells per mm2)')+
+  theme(plot.title = element_text(hjust=0.5))+
+  scale_color_brewer('set 2')
+
+#ggsave("/Users/tanyabrown/Rasmussen lab/Data analysis/filename.pdf", width = 6, height = 4, dpi = 300)
+ggsave("/Users/goocam21/Desktop/Hagaromo/mm2xgeno_CGSmall.pdf", width = 6, height = 4, dpi = 300)
+
+##Draw plot of Small Area MC Density Combined data
+
+hag_smalldata<-read_excel("210311-hagaromo-MC.xlsx - Raw_data.xlsx",
+                          'CombSmall',
+                          col_names = TRUE)
+head(hag_smalldata)
+
+hag_scale_sep <- hag_smalldata %>%
+  separate('fish_name',
+           into = c('exp_date', 'transgene', 'genotype', 'fish_id'), 
+           sep = "-" )
+
+g_order <- c("hag.sibs", "hag.mut")
 
 hag_scale_sep %>% 
   ggplot(aes(x = factor(genotype, lev = g_order), scale_density, fill = genotype)) +
@@ -249,11 +273,12 @@ hag_scale_sep %>%
         panel.grid.minor = element_blank(), 
         panel.background = element_blank(),
         axis.line = element_line(colour = 'black'))+
-  labs(title = 'zMC Cell Density per Scale', x = 'Genotype', y = 'zMC Density (cells per mm2)')+
+  labs(title = 'zMC Cell Density per Scale (Small Area)', x = 'Genotype', y = 'zMC Density (cells per mm2)')+
   theme(plot.title = element_text(hjust=0.5))+
   scale_color_brewer('set 2')
 
 #ggsave("/Users/tanyabrown/Rasmussen lab/Data analysis/filename.pdf", width = 6, height = 4, dpi = 300)
+ggsave("/Users/goocam21/Desktop/Hagaromo/mm2xgeno_CombinedSmall.pdf", width = 6, height = 4, dpi = 300)
 ```
 ```{r}
 # perform mann-whitney test between genotypes on large data
